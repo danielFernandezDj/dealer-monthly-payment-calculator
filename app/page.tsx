d6248ab0a462d6fe4972ddef3e9674d7e463477b
Daniel Fernandez <mdanif9728@gmail.com>
2026-06-30 09:59:09 -0500

    Ading some card to the form-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
+import CalculatorForm from "@/components/calculator-form"
+import { CarFront } from 'lucide-react';
+import Drawer from "@/components/ui/drawer"
 
 export default function Page() {
-
-
-
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-          <p>New branch created.</p>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <div className="flex min-h-svh flex-col bg-slate-100">
+      {/* Header */}
+      <div className="flex w-full py-5 gap-2 justify-center items-center bg-blue-950">
+        <CarFront className="w-12 h-12 text-white" />
+        <p className="text-xl font-bold text-white">Dealer Payment <br /> Calculator</p>
+      </div>
+
+      {/* Main Content */}
+      <div className="b-4 flex w-full border-2">
+        <CalculatorForm />
       </div>
     </div>
   )
---
+++ components/calculator-form.tsx
@@ -0,0 +1,174 @@
+import { Button } from "./ui/button"
+import { Label } from "./ui/label"
+import { Card } from "./ui/card"
+import { Input } from "./ui/input"
+
+import { CarFront, UserRound } from "lucide-react"
+
+export default function CalculatorForm() {
+  return (
+    <form className="flex h-full w-full flex-col gap-4 p-2 text-black">
+      {/* Vehicle Information */}
+      <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
+        <div className="flex items-center gap-2">
+          <CarFront className="h-6 w-auto text-blue-700" />
+          <p>1. VEHICLE</p>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <p>Vehicle Price</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="35.000"
+              type="number"
+              required
+              id="car-price"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Customer Cash */}
+      <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
+        <div className="flex items-center gap-2">
+          <UserRound className="h-6 w-auto text-blue-700" />
+          <p>2. CUSTOMER CASH</p>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <p>Down Payment</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="5.000"
+              type="number"
+              required
+              id="down-payment"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Trade In */}
+      <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
+        <div className="flex items-center gap-2">
+          <CarFront className="h-6 w-auto text-blue-700" />
+          <p>3. TRADE-IN</p>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <p>Current Payoff</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="10.000"
+              type="number"
+              required
+              id="current-payoff"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <p>Dealer Trade Offer</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="8.000"
+              type="number"
+              required
+              id="dealer-trade-offer"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+
+        <div className="flex w-full justify-between gap-2 rounded-md border border-green-200 bg-green-50 p-2 text-green-700">
+          <div className="flex flex-col">
+            <p className="text-sm">Trade Equity (Auto)</p>
+            <div className="flex gap-2">
+              <p>$2000</p>
+              <span>-</span>
+              <p>$5999</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pr-4">
+            <h2 className="text-2xl font-bold">
+              <span>+</span>$3,999
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      {/* Taxes and Fees */}
+      <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
+        <div className="flex items-center gap-2">
+          <CarFront className="h-6 w-auto text-blue-700" />
+          <p>4. TAXES & FEES</p>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <p>Sales Tax Rate (%)</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <Input
+              placeholder="6.25"
+              type="number"
+              required
+              id="sales-tax-rate"
+              className="ml-2.5 w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+            <span className="-ml-3 p-0">%</span>
+          </div>
+        </div>
+
+        <div className="flex w-full items-center justify-between">
+          <p>Dealer Fees</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="225"
+              type="number"
+              required
+              id="dealer-fees"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+
+        <div className="flex w-full items-center justify-between">
+          <p>Other Fees</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="0"
+              type="number"
+              required
+              id="other-fees"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Financing Trigger */}
+      <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
+        <div className="flex items-center gap-2">
+          <UserRound className="h-6 w-auto text-blue-700" />
+          <p>5. FINANCING </p>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <p>APR (Annual Percentage Rate)</p>
+          <div className="flex items-center rounded-md border border-gray-300 px-4">
+            <span>$</span>
+            <Input
+              placeholder="5.000"
+              type="number"
+              required
+              id="down-payment"
+              className="w-20 border-0 focus-visible:border-0 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+}
