82be3c230333f80478baba7e6e9da5d3de44836e
Daniel Fernandez <mdanif9728@gmail.com>
2026-08-29 08:11:14 -0500

    feat: Add Login button.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import MathResolution from "@/components/math-resolution"
+import Link from "next/link"
 
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
@@ -77,15 +78,20 @@ export default function DealerMath() {
   return (
     <div className="flex min-h-svh flex-col bg-slate-100">
       {/* Header */}
-      <div className="flex w-full items-center justify-center gap-2 bg-blue-950 py-5">
-        <CarFront className="h-12 w-12 text-white" />
-        <p className="text-xl font-bold text-white">
-          Dealer Payment <br /> Calculator
-        </p>
+      <div className="flex w-full items-center justify-between gap-2 bg-blue-950 py-5 px-4">
+        <div className="flex items-center gap-2">
+          <CarFront className="h-12 w-12 text-white" />
+          <p className="text-xl font-bold text-white">
+            Dealer Payment <br /> Calculator
+          </p>
+        </div>
+        <Link href="#" className="text-white hover:text-gray-300 font-semibold border-2 py-2 px-4 rounded-lg">
+          Login
+        </Link>
       </div>
 
       {/* Main Content */}
-      <form className="flex h-full w-full flex-col gap-4 p-4 text-black lg:w-1/3 lg:m-auto">
+      <form className="flex h-full w-full flex-col gap-4 p-4 text-black lg:m-auto lg:w-1/3">
         {/* Vehicle Information */}
         <div className="flex w-full flex-col gap-4 rounded-lg bg-white p-4 shadow-md">
           <div className="flex items-center gap-2">
